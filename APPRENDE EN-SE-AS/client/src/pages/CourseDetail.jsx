75067119dbc8332c592b791df6f15ebf4ea76bf9
EmilioColds <emiliofriastapia2705@outlook.com>
2024-06-09 17:45:21 -0600

    Setup CourseDetail with videos

---
+++ APPRENDE EN-SE-AS/client/src/pages/CourseDetail.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+
+const CourseDetail = () => {
+    const { id } = useParams();
+    const course = { id, title: 'Course ' + id, description: 'Description of course ' + id };
+
+    return (
+        <DetailContainer>
+            <h2> {course.title} </h2>
+            <p> {course.description} </p>
+            <Video controls>
+                <source src="https://www.youtube.com/watch?v=dQw4w9WgXcQ" type="video/mp4" />
+                Your browser does not support the video tag.
+            </Video>
+        </DetailContainer>
+    );
+};
+
+export default CourseDetail;
+
+const DetailContainer = styled.div`
+    padding: 2rem;
+    text-align: center;
+    background-color: #DECDF5;
+    
+    h2 {
+        margin-bottom: 1rem;
+    }
+
+    p {
+        margin-bottom: 2rem;
+    }
+`;
+
+const Video = styled.video`
+    width: 100%;
+    max-width: 600px;
+    height: auto;
+`;

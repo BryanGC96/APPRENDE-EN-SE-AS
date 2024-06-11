170db009f03e09cddfd7547a6c7fb79a2568ad47
EmilioColds <emiliofriastapia2705@outlook.com>
2024-06-11 16:19:34 -0600

    Created a theme file and signup initial code

---
+++ APPRENDE EN-SE-AS/client/src/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1B998B',
+    },
+    secondary: {
+      main: '#F8F1FF',
+    },
+    background: {
+      default: '#DECDF5',
+    },
+    text: {
+      primary: '#1C1D21',
+    },
+  },
+});
+
+export default theme;

fcbe3f7879bd1ffed36c7822c93204a487ff7827
BryanGC96 <2014104@icum.edu.mx>
2024-06-11 21:52:21 -0600

    Modifying aboutus page

--- APPRENDE EN-SE-AS/client/src/pages/About.jsx
+++ APPRENDE EN-SE-AS/client/src/pages/About.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Grow from '@mui/material/Grow';
 
 function AboutUs() {
-    return (
-      <div>
-        <h1>About Us</h1>
-        <p>Aqui el ABOUT US!</p>
-        {/* Add more content here as needed */}
-      </div>
-    );
-  }
-  
-  export default AboutUs;
+  const [showAvatars, setShowAvatars] = useState(false);
+
+  useEffect(() => {
+    setShowAvatars(true); // Muestra 'showAvatars' una vez que la pagina ya este montada.
+  }, []); // El array vacio es para asegurarce que el efecto solo pase 1 vez!.
+
+  return (
+    <div className='aboutUsContentBox'>
+      <h1 id='tituloEquipo'>Equipo</h1>
+      <Stack className='aboutUsAvatarsContainers' direction="row" spacing={2}>
+        <Grow in={showAvatars} style={{ transformOrigin: '0 0 0' }} timeout={1000}>
+          <Avatar alt="Imagen de perfil Emilio" src="/src/assets/Emilio.png" sx={{ width: 250, height: 250, marginRight: 20 }} />
+        </Grow>
+        <Grow in={showAvatars} style={{ transformOrigin: '0 0 0' }} timeout={1400}>
+          <Avatar alt="Imagen de perfil Fernando" src="/src/assets/Fernando.png" sx={{ width: 250, height: 250, marginRight: 20 }} />
+        </Grow>
+        <Grow in={showAvatars} style={{ transformOrigin: '0 0 0' }} timeout={1800}>
+          <Avatar alt="Imagen de perfil Bryan" src="/src/assets/Bryan.png" sx={{ width: 250, height: 250, marginRight: 20 }} />
+        </Grow>
+        <Grow in={showAvatars} style={{ transformOrigin: '0 0 0' }} timeout={2000}>
+          <Avatar alt="Imagen de perfil Carlos" src="/src/assets/Carlos.png" sx={{ width: 250, height: 250, marginRight: 20 }} />
+        </Grow>
+        <Grow in={showAvatars} style={{ transformOrigin: '0 0 0' }} timeout={2200}>
+          <Avatar alt="Imagen de perfil Poncho" src="/src/assets/Poncho.png" sx={{ width: 250, height: 250, marginRight: 20 }} />
+        </Grow>
+      </Stack>
+    </div>
+  );
+}
+
+export default AboutUs;
